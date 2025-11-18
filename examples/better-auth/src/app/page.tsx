9c4c101f2c3b9a35190cb260454fdf9eb2f38dd9
Léo Pradel <pradel.leo@gmail.com>
2025-11-18 21:21:56 +0100

    start app setup

--- examples/better-auth/src/app/page.tsx
+++ examples/better-auth/src/app/page.tsx
@@ -1,46 +1,82 @@
+"use client";
+
+import { connect, disconnect, request } from "@stacks/connect";
+import { STACKS_MAINNET } from "@stacks/network";
+import { createSiwsMessage } from "sign-in-with-stacks";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
+  const { data: session } = authClient.useSession();
+
+  async function handleConnect() {
+    const connectResult = await connect();
+
+    const stxAddress = connectResult.addresses.find(
+      (address) => address.symbol === "STX",
+    );
+    if (!stxAddress) {
+      throw new Error("No STX address found");
+    }
+
+    const nonce = await getCsrfToken();
+    if (!nonce) {
+      throw new Error("Cannot get CSRF token");
+    }
+
+    const message = createSiwsMessage({
+      address: stxAddress.address,
+      chainId: STACKS_MAINNET.chainId,
+      domain: "example.com",
+      nonce,
+      uri: "https://example.com/path",
+      version: "1",
+    });
+
+    const signResult = await request("stx_signMessage", {
+      message,
+    });
+
+    signIn("credentials", {
+      address: stxAddress.address,
+      message: message,
+      signature: signResult.signature,
+      redirect: false,
+      callbackUrl: "/",
+    });
+  }
+
+  function handleDisconnect() {
+    disconnect();
+  }
+
+  console.log("session", session);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            sign-in-with-stacks + better-auth Example
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          {session ? (
+            <p>better-auth session: {JSON.stringify(session)}</p>
+          ) : null}
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
+          <button
+            type="button"
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={handleConnect}
           >
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Connect wallet
+          </button>
+          <button
+            type="button"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            onClick={handleDisconnect}
           >
-            Documentation
-          </a>
+            Logout
+          </button>
         </div>
       </main>
     </div>
