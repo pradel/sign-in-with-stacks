a4d62a529487ddc6ee6b645be231a29c3eface34
Léo Pradel <pradel.leo@gmail.com>
2025-11-18 22:52:52 +0100

    add missing deps

--- examples/better-auth/src/lib/auth.ts
+++ examples/better-auth/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { siws } from "siws-better-auth";
 
 export const auth = betterAuth({
+  secret: "secret",
   plugins: [
     siws({
       domain: "localhost:3000",
