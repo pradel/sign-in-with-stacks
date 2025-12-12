0b71acbc46612146690e3471f5fac03b0a965e44
Léo Pradel <pradel.leo@gmail.com>
2025-12-12 09:47:50 +0100

    cleanup

--- examples/better-auth/next.config.ts
+++ examples/better-auth/next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    // Get build to work waiting for a new release of next.js
-    // https://github.com/vercel/next.js/issues/86099#issuecomment-3612857492
-    resolveAlias: {
-      pino: "./src/shims/pino.ts",
-      "thread-stream": "./src/shims/thread-stream.ts",
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
